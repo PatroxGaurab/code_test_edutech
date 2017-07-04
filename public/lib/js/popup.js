a413f991f2bce4332084957e54f9103b55fde3ae
PatroxGaurab <gaurabpa1992@gmail.com>
2017-07-04 18:19:47 +0530

    Popup fixed

--- public/lib/js/popup.js
+++ public/lib/js/popup.js
@@ -5,7 +5,7 @@ var modal3 = document.getElementById('myModal3');
 var modal4 = document.getElementById('myModal4');
 var modal5 = document.getElementById('myModal5');
 // Get the button that opens the modal
-var btn1 = document.getElementById("myBtn1");
+var btn1 = $("#myBtn1");
 var btn2 = document.getElementById("myBtn2");
 var btn3 = document.getElementById("myBtn3");
 var btn4 = document.getElementById("myBtn4");
@@ -14,23 +14,23 @@ var btn5 = document.getElementById("myBtn5");
 var span = $(".close");
 var span = $(".sclose");
 // When the user clicks on the button, open the modal 
-btn1.onclick = function() {
-    modal1.style.display = "block";
-}
-btn2.onclick = function() {
-    modal2.style.display = "block";
-}
-btn3.onclick = function() {
-    modal3.style.display = "block";
-}
-btn4.onclick = function() {
-    modal4.style.display = "block";
-}
+$(document).on('click','#myBtn1',function() {
+    $("#myModal1").css('display','block');
+});
+$(document).on('click','#myBtn2',function() {
+    $("#myModal2").css('display','block');
+});
+$(document).on('click','#myBtn3',function() {
+    $("#myModal3").css('display','block');
+});
+$(document).on('click','#myBtn4',function() {
+    $("#myModal4").css('display','block');
+});
 
 if(btn5){
-    btn5.onclick = function() {
-    modal5.style.display = "block";
-}
+	$(document).on('click','#myBtn5',function() {
+	    $("#myModal5").css('display','block');
+	});
 }
 // When the user clicks on <span> (x), close the modal
 /*span.click(function() {
